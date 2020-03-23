2fe26d452d3fabbf5d337fd4ca0cc748d2f0ce10
Yoni Calsin <yonicb20@gmail.com>
2020-03-22 23:43:41 -0500

    feat: 🎸 Created Parse Line Function !

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const ParseLine = (source: string, value?: string) => {
+   let item = {};
+   const arr = source.split(/\-\>/gi);
+   if (arr.length <= 1) {
+      item[source] = value;
+      return item;
+   }
+   const [a, ...r] = arr;
+   item[a] = ParseLine(r.join('->'), value);
+   return item;
+};
