efa4d2fa39ff03b46ab2de12b764ef240962d7d7
Yoni Calsin <yonicb20@gmail.com>
2020-03-22 23:45:42 -0500

    refactor: 💡 Created merge function does it to make merge !

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,30 @@ const IsObject = (src: any): boolean => {
    return false;
 };
 
+const Merge = (...objs: More[]) => {
+   var payload: More = {},
+      source: More,
+      key: string | Number;
+
+   while (objs.length > 0) {
+      source = objs.splice(0, 1)[0];
+      if (IsObject(source)) {
+         for (key in source) {
+            if (source.hasOwnProperty(key)) {
+               const value = source[key];
+               if (IsObject(value)) {
+                  payload[key] = Merge(payload[key] || {}, value);
+               } else {
+                  payload[key] = value;
+               }
+            }
+         }
+      }
+   }
+
+   return payload;
+};
+
 const ParseLine = (source: string, value?: string) => {
    let item = {};
    const arr = source.split(/\-\>/gi);
