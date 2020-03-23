12df6258bfc733e41c589a6bd0ec519e7ce50b39
Yoni Calsin <yonicb20@gmail.com>
2020-03-22 23:46:17 -0500

    refactor: 💡 Created CogenvObject Main function !

--- src/index.ts
+++ src/index.ts
@@ -45,5 +45,25 @@ const ParseLine = (source: string, value?: string) => {
    return item;
 };
 
+const CogenvObject = (data: More, register: Function) => {
+   register({
+      name: '@cogenv/object',
+      version: '1.0.0',
+   });
+
+   if (!data._objects) {
+      return data;
+   }
+
+   data = data._objects;
+   let payload: More = {};
+
+   for (const key in data) {
+      const parsed = ParseLine(key, data[key]);
+      payload = Merge(payload, parsed);
+   }
+   return payload;
+};
+
 export { ParseLine };
 export default CogenvObject;
