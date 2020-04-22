238fd1f2226d86e27e2164da064738a5f01a9b5b
Yoni Calsin <yonicb20@gmail.com>
2020-04-22 15:50:57 -0500

    fix: 🐛 removed merge function !

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,8 @@
+import { Merge } from 'merge-all-objects';
+
 interface More {
    [key: string]: any;
 }
-
-const IsObject = (src: any): boolean => {
-   if (toString.call(src) === '[object Object]') {
-      return true;
-   }
-   return false;
-};
-
-const Merge = (...objs: More[]) => {
-   var payload: More = {},
-      source: More,
-      key: string | Number;
-
-   while (objs.length > 0) {
-      source = objs.splice(0, 1)[0];
-      if (IsObject(source)) {
-         for (key in source) {
-            if (source.hasOwnProperty(key)) {
-               const value = source[key];
-               if (IsObject(value)) {
-                  payload[key] = Merge(payload[key] || {}, value);
-               } else {
-                  payload[key] = value;
-               }
-            }
-         }
-      }
-   }
-
-   return payload;
-};
 
 const ParseLine = (source: string, value?: string) => {
    let item = {};
